2fdecd051c9f26897d447c93490e15b13062a768
edinabwari <edina@example.com>
2022-12-12 11:24:45 -0800

    new

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0.
+ */
+int main (void)
+{
+	char alpha='abcdefghijklmnopqrstuvwxyz';
+	putchar(alpha);
+	putchar('\n');
+	return(0);
+}
