fe8b08e74c2f84e0aa94844ef8aff7c202c24a59
edinabwari <edina@example.com>
2022-12-15 10:05:48 -0800

    new

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,8 @@
  * main- assigns the random number to a variable n
  *if the last digit of n is greater than 5: the string and is greater than 5
  *if the last digit of n is 0: the string and is 0
- *if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0
+ *if the last digit of n is less than 6 and not 0: 
+ *the string and is less than 6 and not 0
  *followed by a new line
 */
 int main(void)
