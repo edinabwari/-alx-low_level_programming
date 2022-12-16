2186e3f3f6b265cf46a675a98d62138512751090
edinabwari <edina@example.com>
2022-12-16 11:21:48 -0800

    new

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,34 +1,17 @@
 #include "main.h"
 /**
- *
- *  * print_line -draws a straight line in the terminal
- *
- *   * @n: the number of times that the special character must be printed
- *
- *    *
- *
- *     * Return: a straight line
- *
- *      */
-
+  * print_line -draws a straight line in the terminal
+* @n: the number of times that the special character must be printed
+ * Return: a straight line
+ */
 void print_line(int n)
-
 {
-
 		int a = 1;
-
-
-
-			while (a <= n)
-
-					{
-
-								_putchar(95);
-
-										a++;
-
-											}
-
-				_putchar('\n');
-
+		
+		while (a <= n)
+		{
+			_putchar(95);
+			a++;
+		}
+		_putchar('\n');
 }
