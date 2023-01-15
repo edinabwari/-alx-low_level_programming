104dde7c3353c86b584679662f20077b41c9e18c
Edina Bwari <edina@example.com>
2023-01-15 23:15:01 +0300

    late work

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+#include<stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *@n: number from count
+ * Return: all natural numbers from n to 98
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+		if (n > 98)
+		{
+			n--;
+		}
+		else
+		{
+			n++;
+		}
+
+	}
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+
+}
