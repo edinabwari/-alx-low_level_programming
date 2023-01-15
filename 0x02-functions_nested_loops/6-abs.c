dc2935928e97d00328c4bda225cac1ec0057a37b
Edina Bwari <edina@example.com>
2023-01-15 23:08:16 +0300

    late work

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *@c: char to check
+ * Return: absolute value of a char
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (c * -1);
+	else
+		return (c);
+}
