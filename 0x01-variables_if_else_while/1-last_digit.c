f5f851e13774e726da896d86f2c923f65d4a8a24
Edina Bwari <edina@example.com>
2023-01-15 22:38:20 +0300

    late work

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,25 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+#include<stdio.h>
+
 /**
- * main- assigns the random number to a variable n
- *if the last digit of n is greater than 5: the string and is greater than 5
- *if the last digit of n is 0: the string and is 0
- *if the last digit of n is less than 6 and not 0: 
- *the string and is less than 6 and not 0
- *followed by a new line
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
-	int m;
+	int last;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
-
-	if (m > 5)
-		printf("Last digit of %d is %d and is greater than 5\n",
-				 n, m);
-	else if (m == 0)
-		printf("Last digit of %d is %d and is 0\n", n, m);
-	else
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				 n, m);
-
+	last = n % 10;
+	if (last > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	if (last == 0)
+		printf("Last digit of %d is %d and is 0\n", n, last);
+	if (last < 6 && last != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,12 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar (ch);
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar (ch);
+		}
+	}
 	putchar('\n');
 	return (0);
 }
