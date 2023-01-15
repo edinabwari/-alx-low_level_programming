160ae8d4083840a445b5125d0625776f8e8d807e
Edina Bwari <edina@example.com>
2023-01-15 23:09:47 +0300

    late work

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *@c: number to check
+ * Return: last digit of a number
+ */
+int print_last_digit(int c)
+{
+
+	c = c % 10;
+	if (c <= 0)
+		c = c * -1;
+	_putchar ('0' + c);
+	return (c);
+}
