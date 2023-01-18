d306588edb931771a07e6a0650a7ff3110dc02eb
Edina Bwari <bwarikwambokaedina@gmail.com>
2023-01-18 04:19:48 +0300

    late work

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,25 @@
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
+int _putchar(char c);
 /**
- * main - check the code
- * 
- * Return: Always 0.
- */
-struct dog
+*struct dog - Initialize struct dog
+*@name:name type char
+*@age: age type float
+*@owner: name of owner tyme char
+*
+*Description: name, age and owner of a dog
+*/
+
+typedef struct dog
 {
-    char *name;
-    float *age;
-    char *owner;
-};
-int main(void)
-{
-    struct dog my_dog;
-}
+	char *name;
+	float age;
+	char *owner;
+} dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
