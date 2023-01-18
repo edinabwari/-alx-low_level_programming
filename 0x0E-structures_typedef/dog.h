14f1b1da28920b6896e180bde4846eaa7b3c26c6
Edina Bwari <bwarikwambokaedina@gmail.com>
2023-01-18 04:12:16 +0300

    late work

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * 
+ * Return: Always 0.
+ */
+struct dog
+{
+    char *name;
+    float *age;
+    char *owner;
+};
+int main(void)
+{
+    struct dog my_dog;
+}
