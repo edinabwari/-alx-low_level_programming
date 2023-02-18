209cd60fbd79b965bf710ae0159ef233ea2df6fe
edinabwari <bwarikwambokaedina@gmail.com>
2023-02-18 14:12:41 +0300

    new file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print the number of arguments
+ * main - print number of arguments
  * @argv: strings containing program name and arguments
- * @argc: number of strings in the argv
- * Return: Always Succes 0 
+ * @argc: number of strings in argv
+ * Return: Always Success 0
  */
 int main(int argc, char *argv[])
 {
-    (void) argv;
-    printf("%d\n", argc -1);
-    return (0);
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
 }
